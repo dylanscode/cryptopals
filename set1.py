ae2f7688abafc9ecf80ea0f5e5a587ddd9cc9b1c
Dylan McGuire <dylan.mcguire@pitt.edu>
2018-09-20 20:50:03 -0400

    everything in set 1 but 7

--- set1.py
+++ set1.py
@@ -1,4 +1,9 @@
 import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import (
+	Cipher, algorithms, modes
+)
+from cryptography.hazmat.primitives import padding
 
 def hex_to_base64(hexstr):
 	return base64.b64encode(hexstr.decode('hex'))
@@ -22,7 +27,52 @@ def break_single_xor(xorbin):
 				score += 2
 			elif c.upper() in "SHRDLU":
 				score += 1
+			elif c in "\x00*":
+				score -= 4
 		if score > best:
 			best = score
 			key = chr(i)
 	return key
+
+def detect_single_xor(xorbin_list):
+	best = 0
+	text = None
+	for xorbin in xorbin_list:
+		for i in range(255):
+			attempt = single_xor(xorbin, chr(i))
+			score = 0
+			for c in attempt:
+				if c.upper() in "ETAOIN":
+					score += 2
+				elif c.upper() in "SHRDLU":
+					score += 1
+				elif c in "\x00*":
+					score -= 4
+			if score > best:
+				best = score
+				text = xorbin
+	return text
+
+def aes_ecb_encrypt(plaintext, key):
+	padder = padding.PKCS7(128).padder()
+	padded_data = padder.update(plaintext) + padder.finalize()
+	cryptor = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend()).encryptor()
+	return cryptor.update(padded_data) + cryptor.finalize()
+
+def aes_ecb_decrypt(ciphertext, key):
+	unpadder = padding.PKCS7(128).unpadder()
+	cryptor = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend()).decryptor()
+	padded_plaintext = cryptor.update(ciphertext) + cryptor.finalize()
+	return unpadder.update(padded_plaintext) + unpadder.finalize()
+
+def detect_ecb(ctext_list):
+	best = 0
+	ecb_text = None
+	for ctext in ctext_list:
+		blocks = [ctext[i:i+16] for i in range(0, len(ctext), 16)]
+		uniq_blocks = set(blocks)
+		score = len(blocks) - len(uniq_blocks)
+		if score > best:
+			best = score
+			ecb_text = ctext
+	return ecb_text
--- solve_set1.py
+++ solve_set1.py
@@ -1,4 +1,5 @@
 import unittest
+import base64
 import set1
 
 class Set1(unittest.TestCase):
@@ -20,10 +21,27 @@ class Set1(unittest.TestCase):
 		ctext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode('hex')
 		self.assertEqual(key, set1.break_single_xor(ctext))
 
+	def test_chal4(self):
+		with open("static/4.txt") as f:
+			lines = map(lambda x: x.decode('hex'), f.read().splitlines())
+		with open("secret/chal4.txt") as f:
+			ptext = f.read()
+		ctext = set1.detect_single_xor(lines)
+		key = set1.break_single_xor(ctext)
+		self.assertEqual(ptext, set1.single_xor(ctext, key))
+
 	def test_chal5(self):
 		data = """Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal"""
 		ctext = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f".decode('hex')
 		self.assertEqual(ctext, set1.single_xor(data, "ICE"))
+
+	def test_chal7(self):
+		with open("static\\7.txt") as f:
+			ctext = base64.b64decode(f.read())
+		with open("secret\\chal7.txt") as f:
+			ptext = f.read()
+		self.assertEqual(ptext, set1.aes_ecb_decrypt(ctext, "YELLOW SUBMARINE"))
+
 if __name__ == "__main__":
 	unittest.main()
